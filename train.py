34d3126c42a67eb5639678ddf98432e13be680d6
xXKeNdAmAmAsTeRXx <frkubala@student.agh.edu.pl>
2025-07-25 16:05:29 +0200

    delete no needed imports form train.py

--- train.py
+++ train.py
@@ -1,17 +1,3 @@
-import os
-
-import numpy as np
-import timm
-import torch
-import torch.nn as nn
-from sklearn.metrics import roc_auc_score, accuracy_score
-
-from config import config
-from dataset_manager.dataset_manager import get_loaders, REF_CLASSES
-from helper_scripts.logs_plots import save_logs_as_plots
-
-import sys
-import time
 import argparse
 
 
