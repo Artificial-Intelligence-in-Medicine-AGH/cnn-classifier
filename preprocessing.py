d2d43b2d615288cb806e1c2d3f951d31ef865291
bajon1 <dmika@student.agh.edu.pl>
2025-07-31 15:25:03 +0200

    creating csv with classes as bitmaps from user csv input, pandas added to requirements

--- preprocessing.py
+++ preprocessing.py
@@ -10,6 +10,7 @@ import shutil
 from PIL import Image
 import torch
 
+import pandas as pd
 
 if len(sys.argv) < 2:
     print("ERROR: please provide a source directory as a argument of the script")
@@ -17,6 +18,28 @@ if len(sys.argv) < 2:
 
 
 REF_CLASSES = config.classes
+LABELS_PATH = config.labels_file_path
+
+data_labels = pd.read_csv(LABELS_PATH)
+
+to_csv_data = {
+    "image_name": [],
+    "bit_map": []
+}
+
+for row in data_labels.itertuples():
+    etiquette = row[2].split("|")
+    bit_map = np.zeros(len(REF_CLASSES))
+    for idx, r in enumerate(REF_CLASSES):
+        if r in etiquette: 
+            bit_map[idx] = 1
+            
+    bit_map_str = "".join(map(str, bit_map.astype(int)))
+    to_csv_data["image_name"].append(row[1])
+    to_csv_data["bit_map"].append(bit_map_str)
+   
+BITMAP_PATH = os.path.join(config.dataset_path, "bit_map_data.csv")
+(pd.DataFrame(to_csv_data)).to_csv(BITMAP_PATH, index = False)
 
 if not None:
     REF_CLASSES.append("No Finding")
