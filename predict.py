ba17e81ee82c664eb08dc61973a007672cf70570
xXKeNdAmAmAsTeRXx <frkubala@student.agh.edu.pl>
2025-08-03 20:06:15 +0200

    predict/predictor innit

--- predict.py
+++ predict.py
@@ -8,9 +8,10 @@ from sklearn.metrics import roc_auc_score, accuracy_score
 
 from config import config
 from dataset_manager.dataset_manager import get_loaders, REF_CLASSES
-from helper_scripts.logs_plots import save_logs_as_plots
+from logger import Logger
 
 import sys
 import time
 
 
+#TODO implement predicting from folder
